309d8fd7449910ac283de4c9e16409789e27cafa
karim <karimelmorsy95@gmail.com>
2023-06-02 21:21:14 +0300

    Add project files.

---
+++ Reiz Tech Assigment/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reiz_Tech_Assigment
+{
+    public class Clock
+    {
+
+        public int hour, minute;
+      
+        public Clock(int hour, int minute)
+        {
+            this.hour = hour;
+            this.minute = minute;
+        }
+
+        public void CalculateAngle()
+        {
+            double hourDegree = (this.hour * 30) + (this.minute * 30 / 60);
+            double minimumDegree = this.minute * 6;
+
+            double difference = Math.Abs(hourDegree - minimumDegree);
+            if (difference > 180)
+            {
+                difference = 360 - difference;
+            }
+            Console.WriteLine($"Angle between hour hand and minute hand is" + " " + difference);
+            Console.ReadLine();
+        }
+
+            
+
+
+
+        
+    }
+}
---
+++ Reiz Tech Assigment/MainClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reiz_Tech_Assigment
+{
+    internal class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            // First question in REIZ TECH HOMEWORK ASSIGNMENT 
+            Console.WriteLine("Enter Hour: ");
+            int hour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Minute: ");
+            int minute = Convert.ToInt32(Console.ReadLine());
+
+            Clock clock = new Clock(hour , minute);
+
+
+            clock.CalculateAngle();
+        }
+    }
+}
